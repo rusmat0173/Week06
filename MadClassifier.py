cd82ce5874bb6336658198021b3d8e7656217c4e
Rusmat Ahmed <rusmat.ahmed@virginmedia.com>
2018-07-29 19:06:54 +0900

    First go at a class, fun!

--- MadClassifier.py
+++ MadClassifier.py
@@ -8,3 +8,5 @@ I will create an array where if 0.3*X0 + 0.4*X1 >= 10, Y = 1 else Y = -1
 import numpy as np
 
 # create a dataset array
+random_array = np.random.randint(0,16,(4,4))
+print(random_array)
