34439f5cb3b7015e86034ccca6e68b401c1d566e
Rusmat Ahmed <rusmat.ahmed@virginmedia.com>
2018-07-29 17:39:33 +0900

    First go at a class, fun!

---
+++ While_Practice.py
@@ -0,0 +1,29 @@
+"""
+Quick while practice
+"""
+# count = 0
+# goes = 0
+# while count < 50:
+#     new_num = int(input('>'))
+#     count += new_num
+#     goes += 1
+# print(count, goes)
+
+
+# while True:
+#     new_num = int(input('>'))
+#     if new_num == 5:
+#         pass
+#     else:
+#         break
+# print(new_num)
+
+
+while True:
+    line = input('> ')
+    if line[0] == '#':
+        continue
+    if line.lower() == 'done':
+        break
+    print(line)
+print('Done!')
