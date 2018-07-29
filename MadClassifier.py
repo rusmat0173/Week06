fb9cdfdeb9c0e44756b9a2b33f6b5515f01c924b
Rusmat Ahmed <rusmat.ahmed@virginmedia.com>
2018-07-29 23:10:26 +0900

    First go at a class, fun!

--- MadClassifier.py
+++ MadClassifier.py
@@ -1,11 +1,12 @@
 """
 Build my own super simple Machine Learning Classifier!?!
 
-I will create an array where if 0.3*X0 + 0.4*X1 >= 10, Y = 1 else Y = -1
+I will create an array where if (e.g.) 0.5*X0 + 0.6*X1 >= 5, Y = 1 else Y = -1
 
 """
 # imports
 import numpy as np
+import matplotlib.pyplot as plt
 
 # numpy experimentation
 # data = np.random.randint(0,9,(50,2))
@@ -53,8 +54,48 @@ data = create_full_data(predictors, cat_targets)
 print(data[:5])
 
 
+# now the hard part, making a MadClassifier
+class Perceptron(object):
+    """
+    abc
+    """
+    def __init__(self, eta=0.01, n_iter=50, random_state=100):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1+ X.shape[1])
+        self.errors_ = []
+
+        for  _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X,y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self, X):
+        return (np.dot(X, self.w_[1:]) + self.w_[0])
+
+    def predict(self, X):
+        return np.where(self.net_input(X) >=0, 1, -1)
 
 
+# test on the dataset
+X = predictors
+y = cat_targets
+print(X.shape, y.shape)
 
+ppn = Perceptron(eta=0.1, n_iter=10)
+ppn.fit(X,y)
 
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of updates')
+plt.show()
 
