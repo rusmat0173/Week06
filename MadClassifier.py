d0b4179cd8adff3e3fcb15f2b3cd1a65e17e7dd9
Rusmat Ahmed <rusmat.ahmed@virginmedia.com>
2018-07-29 18:52:42 +0900

    First go at a class, fun!

---
+++ MadClassifier.py
@@ -0,0 +1,10 @@
+"""
+Build my own super simple Machine Learning Classifier!?!
+
+I will create an array where if 0.3*X0 + 0.4*X1 >= 10, Y = 1 else Y = -1
+
+"""
+# imports
+import numpy as np
+
+# create a dataset array
